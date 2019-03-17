01d7513dd18a003dcbf3f26b9b067dacf0b99862
Sergiu221 <sergiu221@gmail.com>
2019-03-17 14:14:21 +0100

    Add server as a seprarate repository

--- server/src/main/java/com/sergiu/repository/CandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sergiu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sergiu.entity.CandidateEntity;
-
-public interface CandidateRepository extends JpaRepository<CandidateEntity, Integer> {
-
-}
--- server/src/main/java/com/sergiu/repository/HallRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sergiu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sergiu.entity.HallEntity;
-
-public interface HallRepository extends JpaRepository<HallEntity, Integer> {
-
-}
